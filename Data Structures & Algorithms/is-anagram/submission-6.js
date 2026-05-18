8a416847ad3eaab39730c52cc6c1e1e4ae53c34d
Sid <sidharthv91@gmail.com>
2026-05-18 20:05:09 -0300

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        const freqMap = {};
+        let result = true;
+        if(s.length !== t.length) return false
+        for(const char of s) {
+            freqMap[char] = (freqMap[char] || 0) + 1;
+        }
+        for(const char of t) {
+            if(char in freqMap) {
+                freqMap[char] = freqMap[char] - 1;
+            }
+        }
+        Object.keys(freqMap).forEach((key) => {
+            console.log(key);
+            if(freqMap[key] !== 0) {
+                result = false;
+            }
+        });
+        return result;
+    }
+}
